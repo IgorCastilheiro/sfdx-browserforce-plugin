6f9d623ad0e7fa6efa84a01182ea9d7b54721d71
Igor <igor.castilheiro@atile.com.br>
2019-10-24 14:13:41 -0300

    Community subdomain validation

--- src/plugins/communities/index.ts
+++ src/plugins/communities/index.ts
@@ -9,11 +9,13 @@ const SELECTORS = {
   ENABLE_CHECKBOX: 'input[id$=":enableNetworkPrefId"]',
   DOMAIN_NAME: 'span.sampleArea',
   DOMAIN_NAME_INPUT_TEXT: 'input[id$=":inputSubdomain"]',
+  DOMAIN_AVAILABILITY_BUTTON: 'input[id$=":checkAvailability"]',
+  DOMAIN_REGISTRATION_ERROR: 'span.errorMsg',
   SAVE_BUTTON: 'input[id$=":saveId"]'
 };
 
 export default class Communities extends BrowserforcePlugin {
-  public async retrieve() {
+  public async retrieve(value) {
     const page = await this.browserforce.openPage(PATHS.BASE, {
       waitUntil: ['load', 'domcontentloaded', 'networkidle0']
     });
@@ -21,7 +23,7 @@ export default class Communities extends BrowserforcePlugin {
       page,
       SELECTORS.BASE
     );
-    const response = {};
+    let response = {};
     const inputEnable = await frameOrPage.$(SELECTORS.ENABLE_CHECKBOX);
     if (inputEnable) {
       response['enabled'] = await frameOrPage.$eval(
@@ -30,9 +32,21 @@ export default class Communities extends BrowserforcePlugin {
       );
     } else {
       // already enabled
-      response['enabled'] = true;
+      response = value;
+      await frameOrPage.waitFor(SELECTORS.DOMAIN_NAME);
+      var domain = await frameOrPage.$eval(
+        SELECTORS.DOMAIN_NAME,
+        (el: HTMLSpanElement) => el.innerHTML
+      );
+      domain = domain.match(/<h4>(.*?)<\/h4>/g)[0].replace('<h4>', '').replace('</h4>', '')
+      fs.writeFile('domain.txt', domain, function (err) {
+        if (err) throw err;
+        console.log('[Communities] domain already registred as ' + domain);
+      });
+
     }
     page.close();
+
     return response;
   }
 
@@ -51,8 +65,10 @@ export default class Communities extends BrowserforcePlugin {
       );
 
       const inputEnable = await frameOrPage.$(SELECTORS.ENABLE_CHECKBOX);
+
       if (inputEnable) {
         await frameOrPage.click(SELECTORS.ENABLE_CHECKBOX);
+
         const domainName = (
           config.domainName ||
           this.browserforce.getMyDomain() ||
@@ -60,29 +76,36 @@ export default class Communities extends BrowserforcePlugin {
             .toString(36)
             .substr(2)}`
         ).substring(0, 22);
+
         await frameOrPage.waitFor(SELECTORS.DOMAIN_NAME_INPUT_TEXT);
         await frameOrPage.type(SELECTORS.DOMAIN_NAME_INPUT_TEXT, domainName);
-        page.on('dialog', async dialog => {
-          await dialog.accept();
-        });
-        await frameOrPage.waitFor(SELECTORS.SAVE_BUTTON);
-        await Promise.all([
-          page.waitForNavigation(),
-          frameOrPage.click(SELECTORS.SAVE_BUTTON)
-        ]);
+        await frameOrPage.click(SELECTORS.DOMAIN_AVAILABILITY_BUTTON);
 
-      } else {
-        await frameOrPage.waitFor(SELECTORS.DOMAIN_NAME);
-        var domain = await frameOrPage.$eval(
-          SELECTORS.DOMAIN_NAME,
-          (el: HTMLSpanElement) => el.innerHTML
-        );
-        domain = domain.match(/<h4>(.*?)<\/h4>/g)[0].replace('<h4>', '').replace('</h4>', '')
-        fs.writeFile('domain.txt', domain, function (err) {
-          if (err) throw err;
-          console.log('[Communities] domain file is created successfully.');
-        }); 
-        console.log('[Communities] once communities enabled you can not change the domain name...');
+        await new Promise(resolve => {
+          setTimeout(async () => {
+            var domainTaken = await frameOrPage.$(SELECTORS.DOMAIN_REGISTRATION_ERROR);
+            resolve(domainTaken);
+          }, 500);
+        }).then(async resolvedValue => {
+          if (resolvedValue) {
+            fs.writeFile('domain.txt', "failed", function (err) {
+              if (err) throw err;
+            });
+            await page.close();
+            throw new Error('Domain name registration failed for "' + domainName + '"');
+          } else {
+            console.log('[DEBUG] domain "' + domainName + '" available');
+            page.on('dialog', async dialog => {
+              await dialog.accept();
+            });
+            await frameOrPage.waitFor(SELECTORS.SAVE_BUTTON);
+            await Promise.all([
+                page.waitForNavigation(),
+                frameOrPage.click(SELECTORS.SAVE_BUTTON)
+              ]);
+              await page.close();
+            }
+        })
       }
     }
   }
